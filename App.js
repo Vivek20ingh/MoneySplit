e1ab657a63b54843600b18cf6cb296330af1c111
vivek20ingh <vs2357766@gmail.com>
2021-09-24 01:06:56 +0530

    intail commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {Text} from 'react-native';
+
+
+
+const App= () => {
+  return (
+    <Text>Welcome to MoneySplit App</Text>
+  );
+};
+
+
+export default App;
